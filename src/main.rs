f3edb8d806ac4a6f584a6af7bc4c776630bbb0f6
Nelly <estrelladebelen2000@gmail.com>
2020-12-06 10:10:42 -0400

    Katas 2, segundo refactor

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,19 @@ fn hex_compare(number: i32) -> String {
         number if number == 0 => "0".to_string(),
         number if number == 1 => "1".to_string(),
         number if number == 2 => "2".to_string(),
+        number if number == 3 => "3".to_string(),
+        number if number == 4 => "4".to_string(),
+        number if number == 5 => "5".to_string(),
+        number if number == 6 => "6".to_string(),
+        number if number == 7 => "7".to_string(),
+        number if number == 8 => "8".to_string(),
+        number if number == 9 => "9".to_string(),
+        number if number == 10 => "A".to_string(),
+        number if number == 11 => "B".to_string(),
+        number if number == 12 => "C".to_string(),
+        number if number == 13 => "D".to_string(),
+        number if number == 14 => "E".to_string(),
+        number if number == 15 => "F".to_string(),
         _ => "".to_string(),
     }
 }
@@ -52,5 +65,33 @@ mod tests {
         let decimal = 16;
         let hex = hex_converter(decimal);
         assert_eq!(hex, "10");
+    }
+
+    #[test]
+    fn dec26_to_hex_1_a() {
+        let decimal = 26;
+        let hex = hex_converter(decimal);
+        assert_eq!(hex, "1A");
+    }
+
+    #[test]
+    fn dec31_to_hex_1_f() {
+        let decimal = 31;
+        let hex = hex_converter(decimal);
+        assert_eq!(hex, "1F");
+    }
+
+    #[test]
+    fn dec500_to_hex_1_f4() {
+        let decimal = 500;
+        let hex = hex_converter(decimal);
+        assert_eq!(hex, "1F4");
+    }
+
+    #[test]
+    fn dec1000_to_hex_3_e8() {
+        let decimal = 1000;
+        let hex = hex_converter(decimal);
+        assert_eq!(hex, "3E8");
     }
 }
